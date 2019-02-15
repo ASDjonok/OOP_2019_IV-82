ea13d97666ffd4aa234e15495b66e8a20e528b47
Oleksii Aleshchenko <oleksii.aleshchenko@netcracker.com>
2019-02-15 11:56:27 +0200

    add lab 1

--- src/A.java
+++ src/A.java
@@ -1,8 +1,42 @@
+import java.util.Scanner;
+
 public class A {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        /*Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();*/
+        int a = 2;
+        int b = 1;
+        int n = 3;
+        int m = 2;
+        final int C = 1;
+        double S = 0;
+
+        /*if () {
+
+        }*/
+        boolean flag = true;
+        for (int i = a; i <= n; i++) {
+            if (i == C) {
+                System.out.println("Ділення на нуль:(");
+                flag = false;
+                break;
+            } else {
+                for (int j = b; j <= m; j++) {
+                    S = S + (i + j) / (i - C);
+                }
+            }
+        }
+        if (flag) {
+            System.out.println(S);
+        }
     }
 }
+//var a:integer;
 //begin
 //writeln('Hello World!')
+//read(a);
 //end.
